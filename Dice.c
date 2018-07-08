a8806a12495effe921c90afe77126fba8124f771
s1250230kaito <s1250230@u-aizu.ac.jp>
2018-07-08 23:05:46 +0900

    Greetings added

--- Dice.c
+++ Dice.c
@@ -4,7 +4,13 @@
  
 int main(void){ 
   int s1,s2;
+  char name[10];
   srand(time(NULL));
+
+  printf("What is your name?\n> ");
+  scanf("%s",name);
+
+  printf("Hello, %s!\n",name);
   
   printf("Rolling the dice ...\n");
   
